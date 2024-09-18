71c8c95c2917982d20f64c9efd30e7accc8ae896
drbarros2004 <134460119+drbarros2004@users.noreply.github.com>
2024-09-18 19:22:01 -0300

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -21,6 +21,69 @@ using namespace std;
 
 // Funções para receber o grafo:
 
+// vector<vector<int>> txt_to_adjacency_vector(const string& nome_arquivo, string nome_do_arquivo_de_saida = "resultados.txt") {
+
+//     ifstream arquivo(nome_arquivo);
+//     if (!arquivo.is_open()) {
+//         throw runtime_error("Erro ao abrir o arquivo de entrada!");
+//     }
+
+//     int numVertices, numArestas, u, v, degreeSum;
+//     arquivo >> numVertices;
+//     numArestas = 0;
+//     degreeSum = 0;
+//     vector<int> degrees(numVertices + 1, 0);
+
+//     vector<vector<int>> graph(numVertices + 1); 
+
+//     while (arquivo >> u >> v) {
+//         graph[u].push_back(v);
+//         graph[v].push_back(u);
+
+//         numArestas++;
+//         degrees[u]++;
+//         degrees[v]++;
+//         degreeSum += 2;
+//     }
+
+//     arquivo.close();
+
+//     // Ordenar os graus
+//     sort(degrees.begin() + 1, degrees.end());
+
+//     // Encontrar grau máximo, mínimo e calcular estatísticas
+//     int maxDegree = degrees[numVertices];
+//     int minDegree = degrees[1];
+//     double averageDegree = static_cast<double>(degreeSum) / numVertices;
+//     double medianDegree;
+
+//     // Calcular a mediana do grau
+//     if (numVertices % 2 == 0) {
+//         medianDegree = (degrees[numVertices / 2] + degrees[(numVertices / 2) + 1]) / 2.0;
+//     } else {
+//         medianDegree = degrees[(numVertices + 1) / 2];
+//     }
+
+//     // Gravar os resultados no arquivo de saída
+//     ofstream arquivo_de_saida(nome_do_arquivo_de_saida);
+
+//     if (arquivo_de_saida.is_open()) {
+//         arquivo_de_saida << "Número de vértices: " << numVertices << endl;
+//         arquivo_de_saida << "Número de arestas: " << numArestas << endl;
+//         arquivo_de_saida << "Grau mínimo: " << minDegree << endl;
+//         arquivo_de_saida << "Grau máximo: " << maxDegree << endl;
+//         arquivo_de_saida << "Grau médio: " << averageDegree << endl;
+//         arquivo_de_saida << "Mediana de grau: " << medianDegree << endl;
+//         arquivo_de_saida.close(); 
+//         cout << "Resultados gravados no arquivo " << nome_do_arquivo_de_saida << endl;
+//     } else {
+//         cout << "Erro ao abrir o arquivo de resultados!" << endl;
+//     }
+
+
+//     return graph;
+// }
+
 vector<vector<int>> txt_to_adjacency_vector(const string& nome_arquivo, string nome_do_arquivo_de_saida = "resultados.txt") {
 
     ifstream arquivo(nome_arquivo);
@@ -34,7 +97,7 @@ vector<vector<int>> txt_to_adjacency_vector(const string& nome_arquivo, string n
     degreeSum = 0;
     vector<int> degrees(numVertices + 1, 0);
 
-    vector<vector<int>> graph(numVertices + 1); 
+    vector<vector<int>> graph(numVertices + 1);
 
     while (arquivo >> u >> v) {
         graph[u].push_back(v);
@@ -64,6 +127,48 @@ vector<vector<int>> txt_to_adjacency_vector(const string& nome_arquivo, string n
         medianDegree = degrees[(numVertices + 1) / 2];
     }
 
+    // Função para calcular componentes conexas
+    vector<vector<int>> componentes;
+    vector<int> tamanhos;
+    int quant_componentes = 0;
+    vector<bool> visitados(graph.size(), false);
+    queue<int> fila;
+    int iterador = 0;
+
+    for (int node = 1; node < graph.size(); node++) {
+        if (!visitados[node]) {
+            quant_componentes++;
+            componentes.push_back(vector<int>()); // Inicializa uma nova componente
+            tamanhos.push_back(0); // Inicializa o tamanho da componente
+            fila.push(node);
+            visitados[node] = true;
+            componentes[iterador].push_back(node);
+            tamanhos[iterador]++;
+
+            while (!fila.empty()) {
+                int atual = fila.front();
+                fila.pop();
+
+                for (int vizinho : graph[atual]) {
+                    if (!visitados[vizinho]) {
+                        fila.push(vizinho);
+                        visitados[vizinho] = true;
+                        componentes[iterador].push_back(vizinho);
+                        tamanhos[iterador]++;
+                    }
+                }
+            }
+            iterador++;
+        }
+    }
+
+    sort(componentes.begin(), componentes.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a.size() > b.size();
+    });
+
+    sort(tamanhos.begin(), tamanhos.end());
+    reverse(tamanhos.begin(), tamanhos.end());
+
     // Gravar os resultados no arquivo de saída
     ofstream arquivo_de_saida(nome_do_arquivo_de_saida);
 
@@ -73,18 +178,29 @@ vector<vector<int>> txt_to_adjacency_vector(const string& nome_arquivo, string n
         arquivo_de_saida << "Grau mínimo: " << minDegree << endl;
         arquivo_de_saida << "Grau máximo: " << maxDegree << endl;
         arquivo_de_saida << "Grau médio: " << averageDegree << endl;
-        arquivo_de_saida << "Mediana de grau: " << medianDegree << endl;
-        arquivo_de_saida.close(); 
+        arquivo_de_saida << "Mediana de grau: " << medianDegree << '\n' << endl;
+
+        // Adicionar os resultados das componentes conexas
+        arquivo_de_saida << "Número de componentes conexas: " << quant_componentes << endl;
+        for (int j = 0; j < tamanhos.size(); j++) {
+            arquivo_de_saida << "Componente " << j + 1 << " tem " << tamanhos[j] << " vértices, e eles são: ";
+            for (int i = 0; i < componentes[j].size(); i++) {
+                arquivo_de_saida << componentes[j][i] << " ";
+            }
+            arquivo_de_saida << endl;
+        }
+
+        arquivo_de_saida.close();
         cout << "Resultados gravados no arquivo " << nome_do_arquivo_de_saida << endl;
     } else {
         cout << "Erro ao abrir o arquivo de resultados!" << endl;
     }
 
-
     return graph;
 }
 
-void BFS_tree_vector(const vector<vector<int>>& graph, int origem, ofstream& arquivo_de_saida){
+
+void BFS_tree_vector(const vector<vector<int>>& graph, int origem, string nome_do_arquivo_de_saida){
     int INF = 1e9;
     vector<int> nivel(graph.size(), INF);
     vector<int> pai(graph.size(), INF);
@@ -105,14 +221,21 @@ void BFS_tree_vector(const vector<vector<int>>& graph, int origem, ofstream& arq
                 pai[vizinho] = atual;
             }       
         }       
-    }   
+    }
+
+    ofstream arquivo_de_saida(nome_do_arquivo_de_saida);
+    
     for(int i = 1; i < graph.size(); i++){
         arquivo_de_saida  << "Vértice: " << i << endl;
         arquivo_de_saida << "Nível: " << nivel[i] << endl;
         arquivo_de_saida << "Pai: " << pai[i] << '\n' << '\n';
     }
+
+    cout << "Resultados gravados no arquivo " << nome_do_arquivo_de_saida << endl;
+    arquivo_de_saida.close();
+
+}
     
-} 
 
 void DFS_tree_vector(const vector<vector<int>>& graph, int origem, string nome_do_arquivo_de_saida = "arvoreDFS.txt"){
     int INF = 1e9;
@@ -203,6 +326,61 @@ void Distancia_vector(const vector<vector<int>>& graph, int origem, int destino)
     }
     cout << "A distância entre " << origem << " e " << destino << " é: " << distancia << endl;
 } 
+
+void ComponentesConexas_vector(const vector<vector<int>>& graph) {
+    int quant_componentes = 0;
+    vector<vector<int>> componentes;
+    vector<int> tamanhos;
+    int iterador = 0;
+    vector<bool> visitados(graph.size(), false);
+    queue<int> fila;
+
+    for (int node = 1; node < graph.size(); node++) {
+        if (!visitados[node]) {
+            quant_componentes++;
+            componentes.push_back(vector<int>()); // Inicializa uma nova componente
+            tamanhos.push_back(0); // Inicializa o tamanho da componente
+            fila.push(node);
+            visitados[node] = true;
+            componentes[iterador].push_back(node);
+            tamanhos[iterador]++;
+            
+            while (!fila.empty()) {
+                int atual = fila.front();
+                fila.pop();
+                
+                for (int vizinho : graph[atual]) {
+                    if (!visitados[vizinho]) {
+                        fila.push(vizinho);
+                        visitados[vizinho] = true;
+                        componentes[iterador].push_back(vizinho);
+                        tamanhos[iterador]++;
+                    }
+                }
+            }
+            iterador++;
+        }
+    }
+
+    sort(componentes.begin(), componentes.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a.size() > b.size();
+    });
+
+    sort(tamanhos.begin(), tamanhos.end());
+    reverse(tamanhos.begin(), tamanhos.end());
+
+    cout << "O grafo tem " << quant_componentes << " componente(s) conexa(s)." << endl;
+    
+    for (int j = 0; j < tamanhos.size(); j++) {
+        cout << "Componente " << j + 1 << " tem " << tamanhos[j] << " vertices: " << endl;
+        for (int i = 0; i < componentes[j].size(); i++) {
+            cout << componentes[j][i] << " ";
+        }
+        cout << endl; // Para pular para a próxima linha após listar os vértices
+    }
+    cout << '\n';
+}
+
 
 // A PARTIR DAQUI É MATRIZ DE ADJACÊNCIA
 
