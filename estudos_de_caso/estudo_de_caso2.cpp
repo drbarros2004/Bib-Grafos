04025fd2ca85411fecb1be80cd41f5652ac2d5cd
PedroCintra67 <161370894+PedroCintra67@users.noreply.github.com>
2024-09-24 12:00:05 -0300

    Update estudo_de_caso2.cpp

--- estudos_de_caso/estudo_de_caso2.cpp
+++ estudos_de_caso/estudo_de_caso2.cpp
@@ -53,10 +53,10 @@ int main() {
 
     // Distâncias e diâmetros:
 
-    Distancia_vector(grafo2_vector, 10, 20);
-    Distancia_vector(grafo2_vector, 10, 30);
-    Distancia_vector(grafo2_vector, 20, 30);
-    Diametro_vector(grafo2_vector);
+    Distance_vector(grafo2_vector, 10, 20);
+    Distance_vector(grafo2_vector, 10, 30);
+    Distance_vector(grafo2_vector, 20, 30);
+    Diameter_vector(grafo2_vector);
 
     // Desalocar o grafo2_vector
     grafo2_vector = vector<vector<int>>();
