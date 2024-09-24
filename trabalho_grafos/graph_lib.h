7ea1ec60ad70fd697642c28742f9c2da17ee0281
PedroCintra67 <161370894+PedroCintra67@users.noreply.github.com>
2024-09-24 12:02:25 -0300

    Update graph_lib.h

--- trabalho_grafos/graph_lib.h
+++ trabalho_grafos/graph_lib.h
@@ -10,13 +10,6 @@ using namespace std;
 vector<vector<bool>> txt_to_adjacency_matrix(const string& nome_arquivo, string nome_do_arquivo_de_saida_principal = "resultados.txt");
 vector<vector<int>> txt_to_adjacency_vector(const string& nome_arquivo, string nome_do_arquivo_de_saida_principal = "resultados.txt");
 
-// Funções para obter informações básicas do grafo (vetor de adjacência):
-
-int minDegree(const vector<vector<int>>& grafo);
-int maxDegree(const vector<vector<int>>& grafo);
-double averageDegree(const vector<vector<int>>& grafo);
-double medianDegree(const vector<vector<int>>& grafo);
-
 // Funções para percorrer o grafo (vetor de adjacência):
 
 void BFS_tree_vector(const vector<vector<int>>& graph, int origem, string nome_do_arquivo_de_saida = "arvoreBFS.txt");
@@ -26,7 +19,6 @@ void Distance_vector(const vector<vector<int>>& graph, int origem, int destino);
 void Approximate_diameter_vector(const vector<vector<int>>& graph);
 void Diameter_vector(const vector<vector<int>>& graph);
 
-
 // Funções para percorrer o grafo (matriz de adjacência):
 
 void DFS_tree_matrix(const vector<vector<bool>>& matrix, int origem, string nome_do_arquivo_de_saida = "arvoreDFS.txt");
@@ -35,7 +27,6 @@ void BFS_tree_matrix(const vector<vector<bool>>& matrix, int origem, string nome
 void Distance_matrix(const vector<vector<bool>>& matrix, int origem, int destino);
 void Approximate_diameter_matrix(const vector<vector<bool>>& matrix);
 void Diameter_matrix(const vector<vector<bool>>& matrix);
-
 
 // Contabilizando o tempo:
 
